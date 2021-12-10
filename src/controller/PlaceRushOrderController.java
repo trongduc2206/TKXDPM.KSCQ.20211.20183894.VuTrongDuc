3b1de7ff74fe1f8cf3ff88345bb89dee774221e5
Trong Duc <trongduc22062000@gmail.com>
2021-12-10 22:36:31 +0700

    refactoring place rush order controller

--- src/controller/PlaceRushOrderController.java
+++ src/controller/PlaceRushOrderController.java
@@ -12,56 +12,88 @@ import java.util.HashMap;
 import java.util.Random;
 import java.util.logging.Logger;
 
+/**
+ * Class xu ly logic dat hang giao hang nhanh
+ * @author Vu Trong Duc - 20183894
+ * @since 12/10/2021
+ * @version 1.0
+ */
 public class PlaceRushOrderController extends PlaceOrderController{
     /**
      * Just for logging purpose
      */
     private static Logger LOGGER = utils.Utils.getLogger(PlaceRushOrderController.class.getName());
     // Vu Trong Duc - 20183894
+
+    /**
+     * Method kiem tra tinh hop le thong tin giao hang
+     * @param info thong tin don giao hang nhanh
+     * @throws InterruptedException
+     * @throws IOException
+     */
     @Override
     public void validateDeliveryInfo(HashMap<String, String> info) throws InterruptedException, IOException {
-        if(!validateName(info.get("name"))){
-            throw new InterruptedException("Name is invalid");
-        }
-        if(!validateAddress(info.get("address"))){
-            throw new InterruptedException("Address is invalid");
-        }
-        if(!validatePhoneNumber(info.get("phone"))){
-            throw new InterruptedException("Phone number is invalid");
-        }
+        // kiem tra tinh hop le cua ten, dia chi, so dien thoai
+        super.validateDeliveryInfo(info);
+        // kiem tra tinh hop le cua tinh/thanh pho
         if(!validateProvince(info.get("province"))) {
             throw new InterruptedException("Province does not support rush delivery. Please get back to Cart Screen to unselect rush order");
         }
+        // kiem tra tinh hop le cua ngay giao hang
         if(!validateDeliveryDate(info.get("date"))) {
             throw new InterruptedException("Delivery date must be after today");
         }
     }
     //Vu Trong Duc - 20183894
+
+    /**
+     * Method kiem tra tinh hop le cua thong tin tinh/thanh pho
+     * @param province ten tinh/thanh pho
+     * @return tinh hop le cua thong tin (dang boolean)
+     */
     public boolean validateProvince(String province){
+        //kiem tra ten tinh/thanh pho khac null hoac rong
         if(province == null || province.isEmpty()){
             return false;
         }
+        //kiem tra ten tinh/thanh pho la Ha Noi
         if(!province.equals("Hà Nội")) {
             return false;
         }
         return true;
     }
-
+    //Vu Trong Duc - 20183894
+    /**
+     * Method kiem tra tinh hop le cua thong tin ngay giao hang
+     * @param date ngay giao hang
+     * @return tinh hop le cua ngay giao hang (dang boolean)
+     */
     public boolean validateDeliveryDate(String date){
+        //kiem tra ngay giao hang khac null
         if(date == null) {
             return false;
         }
+        //kiem tra ngay giao hang sau ngay hien tai
         if(LocalDate.now().isAfter(LocalDate.parse(date)) || LocalDate.now().isEqual(LocalDate.parse(date))) {
             return false;
         }
         return true;
     }
-
+    //Vu Trong Duc - 20183894
+    /**
+     * Method tinh phi van chuyen cho don giao hang nhanh
+     * @param order don hang
+     * @return phi van chuyen (dang int)
+     */
     @Override
     public int calculateShippingFee(Order order) {
-        Random rand = new Random();
-        int fees = (int) (((rand.nextFloat() * 10) / 100) * order.getAmount()) + 10000* order.getlstOrderMedia().size();
-        LOGGER.info("Order Amount: " + order.getAmount() + " -- Shipping Fees: " + fees);
-        return fees;
-    }
+        // tinh phi van chuyen
+        int fees = super.calculateShippingFee(order);
+        // tinh phi giao hang nhanh cong them
+        int rushOrderFee = 10000 * order.getlstOrderMedia().size();
+        // tinh tong phi
+        int totalFees = fees + rushOrderFee;
+        LOGGER.info("Order Amount: " + order.getAmount() + " -- Shipping Fees: " + totalFees);
+        return totalFees;
+    } // Vu Trong Duc - 20183894
 }
